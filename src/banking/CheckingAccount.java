3fcbbf68d4340740584c8d36b22a7cd038e0dfa2
Rayan <rb.raies@gmail.com>
2026-01-02 21:28:48 +0300

    added implementation on  CheckingAccountn class

---
+++ src/banking/CheckingAccount.java
@@ -0,0 +1,19 @@
+package banking;
+
+public class CheckingAccount extends Account {
+
+    private double fee;
+
+    public CheckingAccount(String accountNumber, double fee) {
+        super(accountNumber);
+        this.fee = fee;
+    }
+
+    @Override
+    public void withdraw(double amount) {
+        double total = amount + fee;
+        if (total <= balance) {
+            balance -= total;
+        }
+    }
+}
