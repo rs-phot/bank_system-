c3e73ef937ea2546c7a92e2132716062dcedb85e
Rayan <rb.raies@gmail.com>
2026-01-03 19:03:33 +0300

    added transaction class

---
+++ src/banking/Transaction.java
@@ -0,0 +1,34 @@
+package banking;
+
+import java.util.Date;
+
+public class Transaction {
+
+    private int id;
+    private String type;
+    private double amount;
+    private Date date;
+
+    public Transaction(int id, String type, double amount, Date date) {
+        this.id = id;
+        this.type = type;
+        this.amount = amount;
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+}
