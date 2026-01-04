bd3071628b9623b81b45966755bb09ea640f3599
Rayan <rb.raies@gmail.com>
2026-01-04 21:29:35 +0300

    implemented transfer logic in atmservice  with Transferable

--- src/banking/service/ATMService.java
+++ src/banking/service/ATMService.java
@@ -2,9 +2,9 @@ package banking.service;
 import banking.model.Account;
 import banking.model.Transaction;
 import java.util.Date;
+import banking.service.Transferable;
 
-public class ATMService {
-
+public class ATMService implements Transferable {
     public void deposit(Account account, double amount) {
         if (amount <= 0) return;
 
@@ -16,10 +16,8 @@ public class ATMService {
                 amount,
                 new Date()
         );
-
         account.addTransaction(transaction);
     }
-
     public void withdraw(Account account, double amount) {
         if (amount <= 0) return;
 
@@ -39,6 +37,34 @@ public class ATMService {
         return account.getBalance();
     }
 
+    @Override
+public void transfer(Account from, Account to, double amount) {
+
+    if (amount <= 0) return;
+
+    if (from.getBalance() >= amount) {
+
+        from.withdraw(amount);
+        to.deposit(amount);
+
+        Transaction outTx = new Transaction(
+            3,
+            "TRANSFER_OUT",
+            amount,
+            new Date()
+        );
+
+        Transaction inTx = new Transaction(
+            4,
+            "TRANSFER_IN",
+            amount,
+            new Date()
+        );
+
+        from.addTransaction(outTx);
+        to.addTransaction(inTx);
+    }
+}
     public void viewTransactions(Account account) {
         for (Transaction t : account.getTransactions()) {
             System.out.println(
