86c29e4e118d9b45a93db702a18a65b6f6618961
Rayan <rb.raies@gmail.com>
2026-01-04 18:36:48 +0300

    added authentication service class

---
+++ src/banking/service/AuthService.java
@@ -0,0 +1,28 @@
+package banking.service;
+
+import banking.model.User;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AuthService {
+
+    private List<User> users;
+
+    public AuthService() {
+        users = new ArrayList<>();
+    }
+
+    public void register(User user) {
+        users.add(user);
+    }
+
+    public User login(String username, String password) {
+        for (User user : users) {
+            if (user.getUsername().equals(username)
+                    && user.getPassword().equals(password)) {
+                return user;
+            }
+        }
+        return null;
+    }
+}
