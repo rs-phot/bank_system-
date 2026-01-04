50956346c1235e333a17a33c1ef409a3bc731c9a
Rayan <rb.raies@gmail.com>
2026-01-04 20:03:19 +0300

    add App class demonstrating authentication and session flow

---
+++ src/banking/app/App.java
@@ -0,0 +1,41 @@
+package banking.app;
+
+import banking.model.User;
+import banking.service.AuthService;
+import banking.service.Session;
+
+public class App {
+
+    public static void main(String[] args) {
+
+   
+        AuthService authService = new AuthService();
+        Session session = new Session();
+
+        
+        User user = new User("ahmed", "1234", "CUSTOMER");
+        authService.register(user);
+
+     
+        User loggedUser = authService.login("ahmed", "1234");
+
+        if (loggedUser != null) {
+            session.login(loggedUser);
+            System.out.println("Login successful");
+        } else {
+            System.out.println("Login failed");
+        }
+
+       
+        if (session.isLoggedIn()) {
+            System.out.println("Current user: "
+                    + session.getCurrentUser().getUsername());
+        }
+
+      
+        session.logout();
+        System.out.println("User logged out");
+
+        System.out.println("Is logged in? " + session.isLoggedIn());
+    }
+}
