534617f6915238b95758ab0a4ddd491f18a0366c
Rayan <rb.raies@gmail.com>
2026-01-04 20:51:06 +0300

    implemented viewing transactions for accounts

--- src/banking/model/Account.java
+++ src/banking/model/Account.java
@@ -1,19 +1,34 @@
 package banking.model;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public abstract class Account {
 
     protected String accountNumber;
     protected double balance;
+    protected List<Transaction> transactions;
+
 
     public Account(String accountNumber) {
         this.accountNumber = accountNumber;
         this.balance = 0.0;
+        this.transactions = new ArrayList<>();
+
     }
+
+    public void addTransaction(Transaction transaction) {
+    transactions.add(transaction);
+}
+
+public List<Transaction> getTransactions() {
+    return transactions;
+}
+
 
     public String getAccountNumber() {
     return accountNumber;
 }
-
 
     public void deposit(double amount) {
         balance += amount;
